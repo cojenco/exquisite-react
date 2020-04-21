277b6b1c20c88b8ae16ce33c283aa3b277ff95b3
cojenco <icathyouyang@gmail.com>
2020-04-21 16:34:27 -0700

    add function to parse the line from props

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './RecentSubmission.css';
 
 const RecentSubmission = (props) => {
+
+  const lastLine = Object.values(props.mostRecentLine).join(' ');
+
   return (
     <div className="RecentSubmission">
       <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{  }</p>
+      <p className="RecentSubmission__submission">{lastLine}</p>
     </div>
   );
+}
+
+RecentSubmission.propTypes = {
+  mostRecentLine: PropTypes.object.isRequired,
 }
 
 export default RecentSubmission;
