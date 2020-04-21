347ce9e9b88af13dfdbc9a32a7b787238e789846
cojenco <icathyouyang@gmail.com>
2020-04-21 16:16:21 -0700

    add Event Handler in FinalPoem and use props

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,20 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './FinalPoem.css';
 
 const FinalPoem = (props) => {
 
+  const onRevealClick = () => {
+    console.log('REVEAL!');
+    props.onRevealCallback();
+  };
+
+  const readPoem = props.poem.map((line, i) => { 
+    const newLine = Object.values(line).join(' ');
+    return <p key={i}>{newLine}</p>;
+  });
+  // reference: https://www.freecodecamp.org/forum/t/newline-in-react-string-solved/68484/9
+  
   return (
     <div className="FinalPoem">
-      <section className="FinalPoem__poem">
+      {props.revealStatus &&
+      <section className="FinalPoem__poem">  
         <h3>Final Poem</h3>
+        <p>{readPoem}</p>
 
       </section>
+      }
 
+      {!props.revealStatus &&
       <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
+        <input 
+          type="button" 
+          value="We are finished: Reveal the Poem" 
+          className="FinalPoem__reveal-btn" 
+          onClick={onRevealClick}
+        />
       </div>
+      }
     </div>
   );
+}
+
+FinalPoem.propTypes ={
+  poem: PropTypes.array.isRequired,
+  onRevealCallback: PropTypes.func.isRequired,
+  revealStatus: PropTypes.bool.isRequired
 }
 
 export default FinalPoem;
